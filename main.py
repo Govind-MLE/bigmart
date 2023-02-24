49cd3ba7fbdb014c04f5f87c7812b9d09e0f2749
GovindarajMLE <126229848+GovindarajMLE@users.noreply.github.com>
2023-02-24 12:04:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
-url='http://localhost:8080/request'
+url='http://localhost:8080/predict_api'
 r=requests.post(url,json={'Item_Weight':23.6,'Item_MRP':453.5,'Outlet_Location_Type':1})
 print(r.json())
